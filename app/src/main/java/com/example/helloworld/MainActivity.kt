235377ab39257c01dfa04748b4bf922163abf2e8
Hannah Eason <silentvalley.hannah@gmail.com>
2022-12-29 21:10:25 -0600

    Added button click sound and confetti

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.example.helloworld
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.util.Log
 import android.widget.Toast
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
+import nl.dionsegijn.konfetti.KonfettiView
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.helloButton)
+        val viewKonfetti = findViewById<KonfettiView>(R.id.viewKonfetti)
         button.setOnClickListener{
             Log.v("Hello world", "Button clicked!")
-            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_LONG).show()
+            viewKonfetti.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square, Shape.Circle)
+                .addSizes(Size(12))
+                .setPosition(-50f, viewKonfetti.width + 50f, -50f, -50f)
+                .streamFor(300, 5000L)
+            val media = MediaPlayer.create(applicationContext, R.raw.click)
+            media.start()
+
         }
     }
 }
